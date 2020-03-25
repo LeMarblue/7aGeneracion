776191b358e950e0a6485a5727d6daf852c18a28
Mar0903 <cristiangm96@hotmail.com>
2020-03-25 16:28:17 -0600

    add event lisener button save, and xhttp request post

--- practica 25-marzo-formKoders/js/main.js
+++ practica 25-marzo-formKoders/js/main.js
@@ -4,3 +4,14 @@ function getKoderInfo() {
     let koderEmail = document.getElementById("koder-email").value
     return { koderName, koderPhone, koderEmail }
 }
+
+function addButtonSaveListener() {
+    let saveKoderButton = document.getElementById("btn-save-koder")
+    saveKoderButton.addEventListener("click", (event) => {
+        let koderInfo = getKoderInfo()
+        let xhttp = new XMLHttpRequest()
+        xhttp.open("POST", "https://javascript-ajax-d0ce6.firebaseio.com/marlene/koders/.json", true)
+        xhttp.send(JSON.stringify(koderInfo))
+    })
+}
+addButtonSaveListener()
