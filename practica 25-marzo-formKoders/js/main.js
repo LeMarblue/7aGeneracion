52dae48ae4c0d3ff111f0a39e7a3594a1cda5c79
Mar0903 <cristiangm96@hotmail.com>
2020-03-25 19:12:05 -0600

    practices- finished

--- practica 25-marzo-formKoders/js/main.js
+++ practica 25-marzo-formKoders/js/main.js
@@ -1,3 +1,5 @@
+fetchKoders()
+
 function getKoderInfo() {
     let koderName = document.getElementById("koder-name").value
     let koderPhone = document.getElementById("koder-phone").value
@@ -10,8 +12,74 @@ function addButtonSaveListener() {
     saveKoderButton.addEventListener("click", (event) => {
         let koderInfo = getKoderInfo()
         let xhttp = new XMLHttpRequest()
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+                fetchKoders()
+            }
+        }
         xhttp.open("POST", "https://javascript-ajax-d0ce6.firebaseio.com/marlene/koders/.json", true)
         xhttp.send(JSON.stringify(koderInfo))
+
     })
 }
 addButtonSaveListener()
+
+function fetchKoders() {
+    let xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            let response = JSON.parse(this.responseText)
+            console.log(response)
+            printKoderTable(response)
+        }
+    }
+    xhttp.open("GET", "https://javascript-ajax-d0ce6.firebaseio.com/marlene/koders/.json", true)
+    xhttp.send()
+}
+
+function printKoderTable(object) {
+    document.getElementById("table-wrapper").innerHTML = ""
+    let hash
+    let index = 0
+    for (hash in object) {
+        index++
+        let koder = object[hash]
+        let koderName = koder.koderName
+        let koderPhone = koder.koderPhone
+        let koderEmail = koder.koderEmail
+        let currentContent = document.getElementById("table-wrapper").innerHTML
+        let newContent = `
+        <tr>
+      <th scope="row">${index}</th>
+      <td>${koderName}</td>
+      <td>${koderPhone}</td>
+      <td>${koderEmail}</td>
+      <td class="btn btn-danger btn-delate" data-btn-delete-hash="${hash}">X</td>
+    </tr>
+        `
+        document.getElementById("table-wrapper").innerHTML = currentContent + newContent
+    }
+    addDeletedButtonListener()
+}
+function addDeletedButtonListener() {
+    let deletedButtons = document.getElementsByClassName("btn-delate")
+    let i
+    for (i = 0; i < deletedButtons.length; i++) {
+        deletedButtons[i].addEventListener("click", (event) => {
+            let koderHash = event.target.dataset.btnDeleteHash
+            deleteKoder(koderHash)
+
+        })
+    }
+}
+function deleteKoder(hash) {
+    let xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            fetchKoders()
+        }
+    }
+    xhttp.open("DELETE", `https://javascript-ajax-d0ce6.firebaseio.com/marlene/koders/${hash}.json`, true)
+
+    xhttp.send()
+}
