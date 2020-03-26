ec5e94bc5e3d7b9a62c872c0c388fd8cc7581042
Mar0903 <cristiangm96@hotmail.com>
2020-03-26 16:27:06 -0600

    practice completed

--- practica-jueves-26-marzo/js/main.js
+++ practica-jueves-26-marzo/js/main.js
@@ -2,16 +2,17 @@
 var kodersArray = []
 // Saca la informacion del koder desde los inputs y regresa un objeto koder
 function insertKoderToArray() {
-    let koderName = document.getElementById("koder-name").value
-    let koderPhone = document.getElementById("koder-phone").value
-    let koderEmail = document.getElementById("koder-email").value
+    let koderName = $("#koder-name").val()
+    let koderPhone = $("#koder-phone").val()
+    let koderEmail = $("#koder-email").val()
     kodersArray.push({ koderName, koderPhone, koderEmail })
 }
 
 // agrega la funcionalidad al boton de save koder 
 function addButtonSaveListener() {
-    let saveKoderButton = document.getElementById("btn-save-koder")
-    saveKoderButton.addEventListener("click", (event) => {
+    let saveKoderButton = $("#btn-save-koder")
+    saveKoderButton.click((event) => {
+        console.log("jmn")
         insertKoderToArray()
         console.log(kodersArray)
         printKoderTable(kodersArray)
@@ -21,12 +22,12 @@ addButtonSaveListener()
 
 //me pinta mi tabla y me la inserta en el html,
 function printKoderTable(array) {
-    document.getElementById("table-wrapper").innerHTML = ""
+    $("#table-wrapper").html("")
     array.forEach((koder, index) => {
         let koderName = koder.koderName
-        let koderPhone = koder.koderphone
+        let koderPhone = koder.koderPhone
         let koderEmail = koder.koderEmail
-        let currentContent = document.getElementById("table-wrapper").innerHTML
+        let currentContent = $("#table-wrapper").html()
         let newContent = `
         <tr>
       <th scope="row">${index}</th>
@@ -36,20 +37,18 @@ function printKoderTable(array) {
       <td class="btn btn-danger btn-delate" data-btn-delete-index="${index}">X</td>
     </tr>
         `
-        document.getElementById("table-wrapper").innerHTML = currentContent + newContent
+        $("#table-wrapper").html(currentContent + newContent)
     })
 
     addDeletedButtonListener()
 }
 //le agrega la funcionalidad a mi boton de delete
 function addDeletedButtonListener() {
-    let deletedButtons = document.getElementsByClassName("btn-delate")
-    let i
-    for (i = 0; i < deletedButtons.length; i++) {
-        deletedButtons[i].addEventListener("click", (event) => {
-            let koderIndex = event.target.dataset.btnDeleteIndex
-            kodersArray.splice(koderIndex, 1)
-            printKoderTable(kodersArray)
-        })
-    }
+    let deletedButtons = $(".btn-delate")
+    deletedButtons.click((event) => {
+        let koderIndex = event.target.dataset.btnDeleteIndex
+        kodersArray.splice(koderIndex, 1)
+        printKoderTable(kodersArray)
+    })
+
 }
