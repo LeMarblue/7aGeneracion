c684e3bb213bd73fe2ff52bc9312f5f2b0c05fc2
Mar0903 <cristiangm96@hotmail.com>
2020-03-27 18:56:45 -0600

    practices completed, add patch

--- practica-jueves-26-marzo-ajax/js/main.js
+++ practica-jueves-26-marzo-ajax/js/main.js
@@ -50,16 +50,17 @@ function printKoderTable(object) {
         let newContent = `
         <tr>
       <th scope="row">${index}</th>
-      <td>${koderName}</td>
-      <td>${koderPhone}</td>
-      <td>${koderEmail}</td>
+      <td id="${hash}-name">${koderName}</td>
+      <td id="${hash}-phone">${koderPhone}</td>
+      <td id="${hash}-email">${koderEmail}</td>
       <td class="btn btn-danger btn-delate" data-btn-delete-hash="${hash}">X</td>
-      <td class="btn btn-warning btn-edit" data-btn-edit-hash"${hash}">EDIT</td>
+      <td class="btn btn-warning btn-edit" data-btn-edit-hash="${hash}" id="${hash}">EDIT</td>
     </tr>
         `
         $("#table-wrapper").html(currentContent + newContent)
     }
     addDeletedButtonListener()
+    addEditButtonListener()
 }
 function addDeletedButtonListener() {
     let deletedButtons = $(".btn-delate")
@@ -76,6 +77,48 @@ function deleteKoder(hash) {
         url: `https://javascript-ajax-d0ce6.firebaseio.com/marlene/koders/${hash}.json`,
         method: "DELETE",
         success: (response) => { fetchKoders() }
+
+    })
+}
+function addEditButtonListener() {
+    let editButtons = $(".btn-edit")
+    editButtons.click(function () {
+        let hash = $(this).data("btnEditHash")
+        if ($(this).text() === "EDIT") {
+            $(this).text("SAVE")
+            $(this).toggleClass("btn-warning btn-success")
+            $(`#${hash}-name`).attr('contenteditable', true)
+            $(`#${hash}-phone`).attr('contenteditable', true)
+            $(`#${hash}-email`).attr('contenteditable', true)
+        }
+        else {
+            $(this).toggleClass("btn-warning btn-success")
+            $(this).text("EDIT")
+            $(`#${hash}-name`).attr('contenteditable', false)
+            $(`#${hash}-phone`).attr('contenteditable', false)
+            $(`#${hash}-email`).attr('contenteditable', false)
+            let newKoderInfo = getNewKoderInfo(hash)
+            patchKoder(newKoderInfo, hash)
+        }
+    })
+}
+
+function getNewKoderInfo(hash) {
+    let koderName = $(`#${hash}-name`).text()
+    let koderPhone = $(`#${hash}-phone`).text()
+    let koderEmail = $(`#${hash}-email`).text()
+    return { koderName, koderPhone, koderEmail }
+}
+
+
+function patchKoder(newInfo, hash) {
+    $.ajax({
+        url: `https://javascript-ajax-d0ce6.firebaseio.com/marlene/koders/${hash}.json`,
+        method: "PATCH",
+        data: JSON.stringify(newInfo),
+        success: (response) => {
+            fetchKoders()
+        }
 
     })
 }
